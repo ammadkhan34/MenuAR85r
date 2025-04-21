2aa7595656819006e9c990a66dea123efdeb2319
Ammad <ammadempg@gmail.com>
2025-04-21 14:18:43 +0100

    menu Based on restaurant

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -4,7 +4,10 @@ using TMPro;
 
 public class menu : MonoBehaviour
 {
-    string[,] dishNames = {{"cheese toastie","Onion Pie"},{"lemon tart","summer jelly"},{"scoth egg","liver pate"},{"fish","trout"}}; 
+    string[][] dishNames = new string [][] { new string[] {"cheese toastie","Onion Pie"},
+    new string[] {"lemon tart","sticky toffee"},
+    new string[] {"scoth egg","liver pate"},
+    new string[] {"fish","trout"} }; 
     GameObject refrenceForButtons;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,11 +21,13 @@ public class menu : MonoBehaviour
 
     public void showItems(int size1 ) {
   GameObject buttonTemplate = transform.GetChild(0).gameObject;
-  int length1 = dishNames.GetLength(size1);
-        for (int i = 0 ;i < length1-1; i++) { 
+  int length1 = dishNames[size1].Length;
+  print("length is");
+  print(length1);
+        for (int i = 0 ;i < length1; i++) { 
             refrenceForButtons = Instantiate(buttonTemplate,transform);
-            refrenceForButtons.transform.GetChild(1).GetComponent<RawImage>().texture = Resources.Load<Texture2D>("cods cheeks");
-            refrenceForButtons.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "" + dishNames[size1,i];
+            refrenceForButtons.transform.GetChild(1).GetComponent<RawImage>().texture = Resources.Load<Texture2D>("" + dishNames[size1][i]);
+            refrenceForButtons.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "" + dishNames[size1][i];
             
         }
     }
