79b95b31049ebf13460fb71de239b58974af161f
Ammad <ammadempg@gmail.com>
2025-04-23 02:55:05 +0100

    assets restaurant development

--- Assets/RestaurantsList.cs
+++ Assets/RestaurantsList.cs
@@ -7,14 +7,19 @@ public class RestaurantsList : MonoBehaviour
     GameObject buttonTemplate;
 
     string[] restaurantsList = {"Hero Restaurant","Meghan's","Diner Inn","Eat my way"};
+
+    string[] restaurantNames = {"Hero","Meghan","nawab"};
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
 buttonTemplate = transform.GetChild(0).gameObject;
-for (int i = 0 ; i < restaurantsList.Length; i++) {
+print(restaurantsList.Length);
+for (int i = 0 ; i < restaurantsList.Length-1; i++) {
     GameObject g = Instantiate(buttonTemplate,transform);
-    g.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "" + restaurantsList[i];
     int index = i;
+    g.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "" + restaurantsList[i];
+    g.transform.GetChild(1).GetComponent<RawImage>().texture = Resources.Load<Texture2D>("" + restaurantNames[index]);
+    
     g.GetComponent <Button>().onClick.AddListener(delegate{buttonPressed(index);});
 } 
 Destroy(buttonTemplate);
